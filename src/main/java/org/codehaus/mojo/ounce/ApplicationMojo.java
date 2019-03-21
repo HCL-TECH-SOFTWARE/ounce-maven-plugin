dac8030cdb8f929407cd38d9f2a2cd56fe19d8a3
Vijayakumar Balasubramaniyan <vijayakumar_b@hcl.com>
2019-03-21 16:19:23 +0530

    Fix incorrect child project's ppf file path stored in paf file.
    While generating the .paf file, the child project's ppf file shall be constructed and stored. But this ppf file path is generated with the application's root folder instead of the project's root folder.
    Note, the ppf file itself gets generated in the right location, but when that information is stored in the paf file it is incorrect.
    
    I noticed that the OunceProjectBean was feeded with the application's path instead of the project's path, which was causing an incorrect ppf file path generation.

--- src/main/java/org/codehaus/mojo/ounce/ApplicationMojo.java
+++ src/main/java/org/codehaus/mojo/ounce/ApplicationMojo.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2007, Ounce Labs, Inc.
  * All rights reserved.
- * (c) Copyright HCL Technologies Ltd. 2017. All rights reserved.
+ * (c) Copyright HCL Technologies Ltd. 2019. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -268,7 +268,7 @@ public class ApplicationMojo
             MavenProject prj = (MavenProject) iter.next();
 
             if (!skipPoms || !prj.getPackaging().equalsIgnoreCase("pom")) {
-				beanProjects.add( new OunceProjectBean(projectDir, prj.getArtifactId()));
+				beanProjects.add( new OunceProjectBean(prj.getBasedir().getPath(), prj.getArtifactId()));
             }
             else
                 getLog().debug( "Skipping Pom: " + prj.getArtifactId() );
